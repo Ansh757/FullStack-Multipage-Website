66ff5d66703b8a212246e1149b3febf0a6d7bfd5
Ansh757 <ansh.malhotra@mail.utoronto.ca>
2022-12-05 15:28:37 -0500

    login done

---
+++ tfc/src/components/User/Profile/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <header>
+            </header>
+
+            <div className="signup-container">
+
+            </div>
+
+            <footer>
+                <h3>© Ansh, Armaan, Giancarlo </h3>
+            </footer>
+        </div>
+    )
+}
+
+export default Profile;
