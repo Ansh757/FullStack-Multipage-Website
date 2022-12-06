b4dcf3f49ed6801cb7311bd82b50594babce0a02
Ansh757 <ansh.malhotra@mail.utoronto.ca>
2022-12-06 03:13:33 -0500

    main page = minor bugs

--- tfc/src/components/User/User-Main/index.js
+++ tfc/src/components/User/User-Main/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-
+import func from './hours'
 // Reference: https://codepen.io/grohit/pen/jObGzdG
 const User_Main = () => {
     return (
@@ -13,10 +13,9 @@ const User_Main = () => {
                     <nav>
                         <ul className="menuItems">
                             <li><a href='#' data-item='Home'>Home</a></li>
-                            <li><a href='#' data-item='About'>About</a></li>
-                            <li><a href='#' data-item='Projects'>Projects</a></li>
-                            <li><a href='#' data-item='Blog'>Blog</a></li>
-                            <li><a href='#' data-item='Contact'>Contact</a></li>
+                            <li><a href='#' data-item='Classes'>Classes</a></li>
+                            <li><a href='#' data-item='Studios'>Studios</a></li>
+                            <li><a href='#' data-item='Subscriptions'>Subscriptions</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -33,13 +32,68 @@ const User_Main = () => {
             <div className="main-container">
                 <div className="section section-1">
                     <h1>
-                        Welcome <span> Bot1212</span> !
+                        Welcome <span> Bot1212</span>!
                     </h1>
                 </div>
                 <hr />
                 <div className="section section-2">
+                    <h1>
+                        Timings
+                    </h1>
 
+                    <div className="opening-hours">
+                        <div className="opening-hours-content section">
+                            <div className="header">
+                                <span id="open-status"><small className="openorclosed">We are</small></span>
+                            </div>
+                            <table className="opening-hours-table">
+                                <tr id="Monday" itemProp="openingHours" title="Open Monday at 9am to 6pm">
+                                    <td>Monday</td>
+                                    <td className="opens">09:00</td>
+                                    <td>-</td>
+                                    <td className="closes">18:00</td>
+                                </tr>
+                                <tr id="Tuesday" itemProp="openingHours" title="Open Tuesday at 9am to 6pm">
+                                    <td>Tuesday</td>
+                                    <td className="opens">09:00</td>
+                                    <td>-</td>
+                                    <td className="closes">18:00</td>
+                                </tr>
+                                <tr id="Wednesday" itemProp="openingHours" title="Open Wednesday at 9am to 6pm">
+                                    <td>Wednesday</td>
+                                    <td className="opens">09:00</td>
+                                    <td>-</td>
+                                    <td className="closes">18:00</td>
+                                </tr>
+                                <tr id="Thursday" itemProp="openingHours" title="Open Thursday at 9am to 8pm">
+                                    <td>Thursday</td>
+                                    <td className="opens">09:00</td>
+                                    <td>-</td>
+                                    <td className="closes">20:00</td>
+                                </tr>
+                                <tr id="Friday" itemProp="openingHours" title="Open Friday at 9am to 6pm">
+                                    <td>Friday</td>
+                                    <td className="opens">09:00</td>
+                                    <td>-</td>
+                                    <td className="closes">18:00</td>
+                                </tr>
+                                <tr id="Saturday" itemProp="openingHours" title="Open Saturday at 10am to 6pm">
+                                    <td>Saturday</td>
+                                    <td className="opens">10:00</td>
+                                    <td>-</td>
+                                    <td className="closes">18:00</td>
+                                </tr>
+                                <tr id="Sunday" itemProp="openingHours" title="Open Sunday at 11am to 4pm">
+                                    <td>Sunday</td>
+                                    <td className="opens">11:00</td>
+                                    <td>-</td>
+                                    <td className="closes">16:00</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
                 </div>
+
                 <hr />
                 <div className="section section-3">
 
