a777cd0c373a4bf25fda2b6a634b0fc1cd2de42b
armaan-mann <armaan.mann@mail.utoronto.ca>
2022-12-06 22:28:17 -0500

    added some subscription forms

--- tfc/src/components/Subscription/Plans.jsx
+++ tfc/src/components/Subscription/Plans.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './Plans.css'
 import {plansTileData} from './plans-data/plan-tile-data'
-// import pic from '../../photos/running.png'
-// import tick from '../../photos/tick.png'
-// import wtick from '../../photos/whiteTick.png'
 import btick from './photos/whiteTick.png'
+import { useNavigate } from "react-router-dom"
+
 
 
 
 const Plans = () => {
+  const navigate = useNavigate();
   return (
     <div className='plans-overlay'>
+        <header>
+            </header>
         <div className='plan-header'>
             <span className='stroke-text'> START YOUR</span>
             <span > FITNESS JOURNEY WITH TFC </span>
@@ -37,9 +39,14 @@ const Plans = () => {
             ))}
         </div>
         <div className='btn-div'>
-            <button className='join-button'>JOIN NOW</button>
+            <button className='join-button' onClick={() => navigate('/get-plan')}>JOIN NOW</button>
         </div>
+
+        <footer>
+                <h3 style={{color:"white"}}>© Ansh, Armaan, Giancarlo </h3>
+            </footer>
     </div>
+
   )
 }
 
