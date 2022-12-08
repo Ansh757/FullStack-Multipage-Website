bfd1eb02940ffac81c971f355f34e967f09eb18e
Ansh757 <ansh.malhotra@mail.utoronto.ca>
2022-12-08 14:16:31 -0500

    profile view finished

--- tfc/src/App.js
+++ tfc/src/App.js
@@ -1,21 +1,18 @@
 import React, { Component }  from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import './App.css';
-
 import Base from "./components/User/Main";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
 import Plans from "./components/Subscription/Plans"
 import ListStudios from "./components/StudiosByLocation"
-import User_Main from "./components/User/User-Main";
-import Profile from "./components/User/Profile";
-import GetPlan from "./components/Subscription/getPlan/getplan"
-import UpdatePlan from "./components/Subscription/update-plan/update-plan"
-import UpdateCard from "./components/Subscription/update-card/update-card"
-import EditProfile from "./components/User/Profile/editProfile/index"
-
-
+import WithRoute from "./components/User/User-Main";
+import Profile from "./components/User/Profile/index";
+import GetPlan from "./components/Subscription/getPlan/getplan";
+import UpdatePlan from "./components/Subscription/update-plan/update-plan";
+import UpdateCard from "./components/Subscription/update-card/update-card";
+import EditProfile from "./components/User/Profile/editProfile/index";
 import APIContext, {useAPIContext} from "./Contexts/APIContext";
 import APIContextTwo, {useAPIContextTwo} from "./Contexts/APIContextTwo";
 
@@ -34,16 +31,17 @@ function App() {
       <BrowserRouter>
           <Routes>
           <Route path='/' element={<div><Base/></div>}/>
-          <Route path='/register' element={<div><Register/></div>}/>
-          <Route path='/login' element={<div><Login/></div>}/>
-          <Route path='/plans' element={<div><Plans/></div>}/>
-          <Route path='/get-plan' element={<div><GetPlan/></div>}/>
-          <Route path='/update-card' element={<div><UpdateCard/></div>}/>
-          <Route path='/update-plan' element={<div><UpdatePlan/></div>}/>
-          <Route path='/studios' element={studios}/>
-          <Route path='/main' element={<div><User_Main/></div>}> </Route>
-          <Route path='/main/profile' element={<div><Profile/></div>}> </Route>
-          <Route path='/main/profile/edit' element={<div><EditProfile/></div>}> </Route>
+          <Route path='register' element={<div><Register/></div>}/>
+          <Route path='login' element={<div><Login/></div>}/>
+          <Route path='plans' element={<div><Plans/></div>}/>
+          <Route path='get-plan' element={<div><GetPlan/></div>}/>
+            <Route path='update-card' element={<div><UpdateCard/></div>}/>
+            <Route path='update-plan' element={<div><UpdatePlan/></div>}/>
+          <Route path='studios' element={studios}/>
+          <Route path='main' element={<div><WithRoute/></div>}> </Route>
+          <Route path=':id/profile/' element={<Profile/>}>
+            <Route path='view' element={<div><EditProfile/></div>}></Route>
+          </Route>
           </Routes>
       </BrowserRouter>
     </div>
