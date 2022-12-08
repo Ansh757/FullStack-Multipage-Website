e5d9725d2429be3ef3940a52f6e6178dd14ac2b3
armaan-mann <armaan.mann@mail.utoronto.ca>
2022-12-08 00:33:07 -0500

    added front for profile

--- tfc/src/components/User/Profile/index.js
+++ tfc/src/components/User/Profile/index.js
@@ -5,104 +5,74 @@ import 'react-bootstrap'
 const Profile = () => {
     return (
         <div className='profile'>
-            <header>
-                <div className="website-logo">
-                    <img src="https://www.cs.toronto.edu/~kianoosh/courses/csc309/resources/images/tfc.png" alt="logo-tfc-picture"/>
+            <div className="card">
+                <div className="ds-top"></div>
+                <div className="avatar-holder">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1820405/profile/profile-512.jpg?1533058950" alt="Albert Einstein"/>
                 </div>
-                <div className="navbar">
-                    <nav>
-                        <ul className="menuItems">
-                            <li><a href='#' data-item='Home'>Home</a></li>
-                            <li><a href='#' data-item='Classes'>Classes</a></li>
-                            <li><a href='#' data-item='Studios'>Studios</a></li>
-                            <li><a href='/plans' data-item='Subscriptions'>Subscriptions</a></li>
-                        </ul>
-                    </nav>
+                <div className="name">
+                    <a href="https://codepen.io/AlbertFeynman/" target="_blank">Albert Feynman</a>
                 </div>
-                <div className="user-logo">
-                    <button className="user-btn">
-                        <i className="fa-solid fa-user"></i>
-                    </button>
-                    <button className="user-btn">
-                        <i className="fa-solid fa-right-from-bracket"></i>
-                    </button>
-                </div>
-
-            </header>
-
-            <div className="container22">
-                <div className="container rounded bg-white mt-5 mb-5">
-                    <div className="row">
-                        <div className="col-md-3 border-right">
-                            <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img
-                                className="rounded-circle mt-5" width="150px"
-                                src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"/><span
-                                className="font-weight-bold">username</span>
-                                <span
-                                className="text-black-50">email address</span>
-                                <span>
-                                </span></div>
-                        </div>
-                        <div className="col-md-5 border-right">
-                            <div className="p-3 py-5">
-                                <div className="d-flex justify-content-between align-items-center mb-3">
-                                    <h4 className="text-right">Profile Settings</h4>
-                                </div>
-                                <div className="row mt-2">
-                                    <div className="col-md-6"><label className="labels">First Name</label><input type="text"
-                                                                                                           className="form-control"
-                                                                                                           placeholder="first name"
-                                                                                                           value=""/>
-                                    </div>
-                                    <div className="col-md-6"><label className="labels">Last Name</label><input
-                                        type="text" className="form-control" value="" placeholder="last name"/></div>
-                                </div>
-                                <div className="row mt-3">
-                                    <div className="col-md-12"><label className="labels">Username</label><input
-                                        type="text" className="form-control" placeholder="Username" value=""/>
-                                    </div>
-                                    <div className="col-md-12"><label className="labels">Phone Number</label><input
-                                        type="text" className="form-control" placeholder="enter phone number" value=""/>
-                                    </div>
-                                    <div className="col-md-12"><label className="labels">Email</label><input
-                                        type="email" className="form-control" placeholder="enter email address" value=""/>
-                                    </div>
-                                    <div className="col-md-12"><label className="labels">Password</label><input
-                                        type="password" className="form-control" placeholder="enter new password"
-                                        value=""/></div>
-                                    <div className="col-md-12"><label className="labels">Repeat Password</label><input
-                                        type="password" className="form-control" placeholder="repeat new password"
-                                        value=""/></div>
-
-                                    <div className="col-md-12"><label className="labels">Avatar</label><input
-                                        type="file" className="form-control" value=""/></div>
-                                </div>
-                                <div className="mt-5 text-center">
-                                    <button className="btn btn-primary profile-button" type="button">Save Profile
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        {/*<div className="col-md-4">*/}
-                        {/*    <div className="p-3 py-5">*/}
-                        {/*        <div className="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span*/}
-                        {/*            className="border px-3 p-1 add-experience"><i*/}
-                        {/*            className="fa fa-plus"></i>&nbsp;Experience</span></div>*/}
-
-                        {/*            <div className="col-md-12"><label className="labels">Experience in Designing</label><input*/}
-                        {/*                type="text" className="form-control" placeholder="experience" value=""/></div>*/}
-                        {/*                <div className="col-md-12"><label className="labels">Additional*/}
-                        {/*                    Details</label><input type="text" className="form-control"*/}
-                        {/*                                          placeholder="additional details" value=""/></div>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
+                
+                <div className="ds-info">
+                    <div className="ds pens">
+                    <h6 title="Number of pens created by the user">Pens <i className="fas fa-edit"></i></h6>
+                    <p>29</p>
                     </div>
-                </div>
-            </div>
+                    <div className="ds projects">
+                    <h6 title="Number of projects created by the user">Projects <i className="fas fa-project-diagram"></i></h6>
+                    <p>0</p>
+                    </div>
+                    <div className="ds posts">
+                    <h6 title="Number of posts">Posts <i className="fas fa-comments"></i></h6>
+                    <p>0</p>
+                    </div>
+                    </div>
+                    <div className="ds-skill">
+                        <h6>Profile Data <i className="fa fa-code" aria-hidden="true"></i></h6>
+                        <div className="skill html">
+                        <h6><i class="fa-solid fa-user-check" style={{color:'pink'}}></i> USERNAME: a-mann </h6>
+                        <div className="bar bar-html">
+                            <p>100%</p>
+                        </div>
+                        </div>
+                        <div className="skill css">
+                        <h6><i class="fa-solid fa-signature" style={{color:'pink'}}></i> FIRST NAME: Armaan  </h6>
+                        <div className="bar bar-css">
+                            <p>100%</p>
+                        </div>
+                        </div>
 
-            <footer>
-                <h3>© Ansh, Armaan, Giancarlo </h3>
-            </footer>
+                        <div className="skill css">
+                        <h6><i class="fa-solid fa-signature" style={{color:'pink'}}></i> LAST NAME: Mann  </h6>
+                        <div className="bar bar-css">
+                            <p>100%</p>
+                        </div>
+                        </div>
+
+                        <div className="skill css">
+                        <h6><i class="fa-solid fa-phone"style={{color:'pink'}}></i> PHONE NUMBER: 111-111-1111  </h6>
+                        <div className="bar bar-css">
+                            <p>100%</p>
+                        </div>
+                        </div>
+
+                        <div className="skill javascript">
+                        <h6><i class="fa-solid fa-envelope" style={{color:'pink'}}></i> EMAIL: a@mail.com </h6>
+                        <div className="bar bar-js">
+                            <p>75%</p>
+                        </div>
+                        </div>
+
+                        <div className="skill javascript">
+                        <h6><i class="fa-solid fa-lock" style={{color:'pink'}} ></i> PASSWORD: mypassword </h6>
+                        <div className="bar bar-js">
+                            <p>75%</p>
+                        </div>
+                        </div>
+
+                    </div>
+                    </div>
         </div>
     )
 }
