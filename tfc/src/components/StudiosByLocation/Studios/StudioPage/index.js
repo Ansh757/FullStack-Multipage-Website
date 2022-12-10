55ccfc0e80c45f9e8c9be6cd3bacab9ee92ad327
gsr227 <82047863+gsr227@users.noreply.github.com>
2022-12-10 12:06:36 -0500

    c

--- tfc/src/components/StudiosByLocation/Studios/StudioPage/index.js
+++ tfc/src/components/StudiosByLocation/Studios/StudioPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import {useLocation, useParams, Link} from "react-router-dom";
 import '../StudioPage/style.css';
 import axios from 'axios';
