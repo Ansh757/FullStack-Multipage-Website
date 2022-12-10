223ae1df22f11feafa005091442055460d60cca5
Ansh757 <ansh.malhotra@mail.utoronto.ca>
2022-12-10 05:25:26 -0500

    starting the schedule view

--- tfc/src/components/Classes/Enrollment/SelectButton/index.js
+++ tfc/src/components/Classes/Enrollment/SelectButton/index.js
@@ -5,9 +5,14 @@ import React from "react";
 const SelectButton = () => {
 
     const { studios } = useContext(APIContext);
+
+    const studioList = () => {
+        for (let i = 0; i < studios.length; i++) {
+            console.log(studios[i]);
+        }
+    }
+
     return <>
-
-
 
     </>
 }
